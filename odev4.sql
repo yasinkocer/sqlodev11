368df3e957ac6d72b14bd9eb09a6d4a52c9cf413
yasinkocer <you@example.com>
2022-10-18 11:05:50 +0300

    first commit

---
+++ odev4.sql
@@ -0,0 +1,27 @@
+--İlk 3 sorguyu tekrar eden veriler için de yapalım.
+
+(
+SELECT first_name FROM actor
+)
+UNION ALL
+(
+SELECT first_name FROM customer
+);
+
+
+(
+SELECT first_name FROM actor
+)
+INTERSECT ALL
+(
+SELECT first_name FROM customer
+);
+
+
+(
+SELECT first_name FROM actor
+)
+EXCEPT
+(
+SELECT first_name FROM customer
+);
